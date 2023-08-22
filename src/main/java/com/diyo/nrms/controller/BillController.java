f59542ee30598fc89a5bebf7ebf8905eef84a4cf
rajee <rajeepoudyal@gmail.com>
2023-08-22 07:46:57 -0400

    'appended-by-rajee'

--- src/main/java/com/diyo/nrms/controller/BillController.java
+++ src/main/java/com/diyo/nrms/controller/BillController.java
@@ -37,6 +37,7 @@ public class BillController {
     }
     @DeleteMapping("/{billId}")
     public ResponseEntity<String> deleteBillById(@PathVariable("billId") Long billId){
+        System.out.println("deleted by Rajee");
         return ResponseEntity.ok(billService.deleteBillById(billId));
     }
     @GetMapping("/{billId}")
